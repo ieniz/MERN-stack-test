8f2ff354a39a8a2db7c75c83340c366233c63d1f
eniz <icanoviceniz@gmail.com>
2024-01-20 20:38:15 +0100

    drugi korak rute

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+return <BrowserRouter> 
+<Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/signin' element={<Signin/>} />
+<Route path='/signup' element={<SignUp/>} />
+<Route path='/about' element={<About/>} />
+<Route path='/profile' element={<Profile/>} />
+
+
+
+
+</Routes>
+</BrowserRouter>
+
 }
 
+
+ 
